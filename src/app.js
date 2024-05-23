578cdb03e64c4d8a14a96c9718e0a02576fe553f
nmoloi <ntshebetsengmoloi@gmail.com>
2024-05-23 14:10:00 +0200

    updated the date and time to access the api information

--- src/app.js
+++ src/app.js
@@ -1,42 +1,3 @@
-let now = new Date();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let weekday = days[now.getDay()];
-let day = document.querySelector("#day");
-day.innerHTML = `${weekday}`;
-
-let hours = now.getHours();
-let minutes = now.getMinutes();
-let time = document.querySelector("#time");
-time.innerHTML = `${hours}:${minutes}`;
-
-let theDate = document.querySelector("#current-date");
-let dayNumber = now.getDate();
-
-let months = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-];
-let month = months[now.getMonth()];
-theDate.innerHTML = `${dayNumber} ${month}`;
-
 let searchForm = document.querySelector("#search-form");
 let inputBar = document.querySelector("#input-bar");
 
@@ -69,6 +30,55 @@ function displayWeather(response) {
   let clouds = document.querySelector("#cloud-cover");
   clouds.innerHTML = response.data.condition.description;
 
-  let emoji = document.querySelector("#emoji");
-  //emoji.innerHTML = response.data.condition.icon_url;
+  let now = new Date(response.data.time * 1000);
+  let day = document.querySelector("#weekday");
+  day.innerHTML = formatWeekday(now);
+
+  let time = document.querySelector("#time");
+  time.innerHTML = formatTime(now);
+
+  let currentDate = document.querySelector("#current-date");
+  currentDate.innerHTML = formatDate(now);
+}
+function formatWeekday(now) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+  return `${day}`;
+}
+
+function formatTime(now) {
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let hours = now.getHours();
+  return `${hours}:${minutes}`;
+}
+
+function formatDate(now) {
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let month = months[now.getMonth()];
+  let dayNumber = now.getDate();
+  return `${dayNumber} ${month}`;
 }
